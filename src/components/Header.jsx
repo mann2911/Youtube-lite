0dba6cd49032eaa7acff75361d3b08074cc07420
mann patel <mannpatel1360@gmail.com>
2023-08-17 20:54:05 +0530

    youtube-lite

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 import React, { useContext, useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 
-import ytLogo from "../images/yt-logo.png";
+import ytLogo from "../images/youtube.png";
 import ytLogoMobile from "../images/yt-logo-mobile.png";
-
+import myImage from '../images/icons8-student-48.png'
 import { SlMenu } from "react-icons/sl";
 import { IoIosSearch } from "react-icons/io";
 import { RiVideoAddLine } from "react-icons/ri";
@@ -43,24 +43,24 @@ const Header = () => {
             <div className="flex h-5 items-center">
                 {pageName !== "video" && (
                     <div
-                        className="flex md:hidden md:mr-6 cursor-pointer items-center justify-center h-10 w-10 rounded-full hover:bg-[#303030]/[0.6]"
+                        className="flex md:hidden md:mr-6 cursor-pointer items-center justify-center w-10 h-10 rounded-full hover:bg-[#303030]/[0.6]"
                         onClick={mobileMenuToggle}
                     >
                         {mobileMenu ? (
-                            <CgClose className="text-white text-xl" />
+                            <CgClose className="text-black text-xl" />
                         ) : (
-                            <SlMenu className="text-white text-xl" />
+                            <SlMenu className="text-black text-xl" />
                         )}
                     </div>
                 )}
                 <Link to="/" className="flex h-5 items-center">
                     <img
-                        className="h-full hidden dark:md:block"
+                        className="h-full hidden md:block"
                         src={ytLogo}
                         alt="Youtube"
                     />
                     <img
-                        className="h-full md:hidden"
+                        className="h-full block md:hidden"
                         src={ytLogoMobile}
                         alt="Youtube"
                     />
@@ -69,11 +69,11 @@ const Header = () => {
             <div className="group flex items-center">
                 <div className="flex h-8 md:h-10 md:ml-10 md:pl-5 border border-[#303030] rounded-l-3xl group-focus-within:border-blue-500 md:group-focus-within:ml-5 md:group-focus-within:pl-0">
                     <div className="w-10 items-center justify-center hidden group-focus-within:md:flex">
-                        <IoIosSearch className="text-white text-xl" />
+                        <IoIosSearch className="text-black text-xl" />
                     </div>
                     <input
                         type="text"
-                        className="bg-transparent outline-none text-white pr-5 pl-5 md:pl-0 w-44 md:group-focus-within:pl-0 md:w-64 lg:w-[500px]"
+                        className="bg-transparent outline-none text-black pr-5 pl-5 md:pl-0 w-44 md:group-focus-within:pl-0 md:w-64 lg:w-[500px]"
                         onChange={(e) => setSearchQuery(e.target.value)}
                         onKeyUp={searchQueryHandler}
                         placeholder="Search"
@@ -84,20 +84,20 @@ const Header = () => {
                     className="w-[40px] md:w-[60px] h-8 md:h-10 flex items-center justify-center border border-l-0 border-[#303030] rounded-r-3xl bg-white/[0.1]"
                     onClick={() => searchQueryHandler("searchButton")}
                 >
-                    <IoIosSearch className="text-white text-xl" />
+                    <IoIosSearch className="text-black text-xl" />
                 </button>
             </div>
             <div className="flex items-center">
                 <div className="hidden md:flex">
                     <div className="flex items-center justify-center h-10 w-10 rounded-full hover:bg-[#303030]/[0.6]">
-                        <RiVideoAddLine className="text-white text-xl cursor-pointer" />
+                        <RiVideoAddLine className="text-black text-xl cursor-pointer" />
                     </div>
                     <div className="flex items-center justify-center ml-2 h-10 w-10 rounded-full hover:bg-[#303030]/[0.6]">
-                        <FiBell className="text-white text-xl cursor-pointer" />
+                        <FiBell className="text-black text-xl cursor-pointer" />
                     </div>
                 </div>
-                <div className="flex h-8 w-8 overflow-hidden rounded-full md:ml-4">
-                    <img src="https://xsgames.co/randomusers/assets/avatars/female/67.jpg" />
+                <div className="flex h-8 w-10  overflow-hidden rounded-full md:ml-4">
+                    <img src={myImage} alt="my-image" />
                 </div>
             </div>
         </div>
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-const BASE_URL = "https://youtube138.p.rapidapi.com";
+const BASE_URL = "https://youtube-v31.p.rapidapi.com";
 
 const options = {
     params: { hl: "en", gl: "US" },
     headers: {
         "X-RapidAPI-Key":
-            process.env.REACT_APP_YOUTUBE_API_KEY || "YOUR_API_KEY",
+            process.env.REACT_APP_YOUTUBE_API_KEY || "bba33a0155msh652203ba3d01cf1p1aee7cjsn8924bb93e6ac",
         "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
     },
 };
@@ -15,3 +15,4 @@ export const fetchDataFromApi = async (url) => {
     const { data } = await axios.get(`${BASE_URL}/${url}`, options);
     return data;
 };
+
